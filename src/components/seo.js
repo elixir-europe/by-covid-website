565db05319bbf949580c2cb7bc24d20286b4393e
Martin Cook <martin.cook@elixir-europe.org>
2021-09-25 11:13:56 +0100

    Fixing casing of component

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-function SEO({ description, lang, meta, image, keywords, title }) {
+function Seo({ description, lang, meta, image, keywords, title }) {
   return (
     <StaticQuery
       query={detailsQuery}
@@ -77,13 +77,13 @@ function SEO({ description, lang, meta, image, keywords, title }) {
   )
 }
 
-SEO.defaultProps = {
+Seo.defaultProps = {
   lang: 'en',
   meta: [],
   keywords: [],
 }
 
-SEO.propTypes = {
+Seo.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.array,
@@ -91,7 +91,7 @@ SEO.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
-export default SEO
+export default Seo
 
 const detailsQuery = graphql`
   query DefaultSEOQuery {
