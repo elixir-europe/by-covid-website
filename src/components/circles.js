8bd2b18e5c1c35c687fae33840574d2a3f2742fd
Martin Cook <martin.cook@elixir-europe.org>
2021-09-29 23:31:27 +0100

    Finish Circles component

--- src/components/circles.js
+++ src/components/circles.js
@@ -1,35 +1,39 @@
 import * as React from 'react'
 import { Container, Row, Col} from 'react-bootstrap'
 
-const Circles = () => {
+const Circles = (props) => {
   return (
     <Container className="full-width">
       <Row>
         <Col xs={6} sm={3} lg={3}>
           <div class="circle mb-4">
             <div class="circle-txt">
-              <span class="fs-3 fw-bold d-block mb-4">12M</span> Euros
+              <span class="fs-3 fw-bold d-block mb-4">{props.circle1Value}</span>
+               {props.circle1}
             </div>
           </div>
         </Col>
         <Col xs={6} sm={3} lg={3}>
           <div class="circle mb-4">
             <div class="circle-txt">
-              <span class="fs-3 fw-bold d-block mb-4">53</span> Partners
+              <span class="fs-3 fw-bold d-block mb-4">{props.circle2Value}</span>
+              {props.circle2}
             </div>
           </div>
         </Col>
         <Col xs={6} sm={3} lg={3}>
           <div class="circle mb-4">
             <div class="circle-txt">
-              <span class="fs-3 fw-bold d-block mb-4">20</span> Countries
+              <span class="fs-3 fw-bold d-block mb-4">{props.circle3Value}</span>
+              {props.circle3}
             </div>
           </div>
         </Col>
         <Col xs={6} sm={3} lg={3}>
           <div class="circle mb-4">
             <div class="circle-txt">
-              <span class="fs-3 fw-bold d-block mb-4">36</span> Months
+              <span class="fs-3 fw-bold d-block mb-4">{props.circle4Value}</span>
+              {props.circle4}
             </div>
           </div>
         </Col>
