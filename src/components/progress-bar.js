cca61031bf02dfaff165f91ba8cf96f3313939ee
Martin Cook <martin.cook@elixir-europe.org>
2021-09-30 23:47:24 +0100

    Finish progress bar

---
+++ src/components/progress-bar.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
+const Progress = (props) => {
+  return (
+    <div>
+      <h2 class="mt-2">{props.progressTitle}</h2>
+      <div class="bar-progress">
+        <ProgressBar now={props.progress} />
+        <p class="progress-start">October 2021 kick-off</p>
+        <p class="progress-end">October 2024</p>
+      </div>
+    </div>
+  )
+}
+
+export default Progress
