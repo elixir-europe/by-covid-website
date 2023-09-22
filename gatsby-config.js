06d228de50e0a578fed53a191096199df63d01bf
Prithviraj Mitra <pmitra@ebi.ac.uk>
2023-09-22 23:38:35 +0100

    netlify deploy 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,6 +106,7 @@ module.exports = {
         path: `./src/data/`,
       },
     },
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
