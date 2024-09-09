d847cacbc85316c4a0fe1ba0b609256eef9a9b7f
Prithviraj Mitra <prithviraj.mitra@elixir-europe.org>
2024-09-09 09:33:10 +0100

    remove extra brackets imageog and twitter

--- src/components/latest-news-row.js
+++ src/components/latest-news-row.js
@@ -29,16 +29,8 @@ const LatestNewsItem = () => (
                 title
                 description
                 class
-                imageTwitter {
-                  childImageSharp {
-                    gatsbyImageData(width: 1024, height: 512, quality: 90)
-                  }
-                }
-                imageOg {
-                  childImageSharp {
-                    gatsbyImageData(width: 1200, height: 630, quality: 90)
-                  }
-                }
+                imageTwitter
+                imageOg
                 imageAlt
                 postType
                 date(formatString: "D MMMM YYYY")
